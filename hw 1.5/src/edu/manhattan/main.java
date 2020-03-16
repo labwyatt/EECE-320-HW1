f0a618c478a01af02c179acffe615bd338dc46e8
labwyatt <38740477+labwyatt@users.noreply.github.com>
2020-03-15 23:36:52 -0400

    Add files via upload

---
+++ hw 1.5/src/edu/manhattan/main.java
@@ -0,0 +1,11 @@
+package edu.manhattan;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Number	^2	^3	^4");
+		for(int i = 1; i < 6; i++) {
+			System.out.println(i + "        " + (i*i) + "       " + (i*i*i) + "       " + (i*i*i*i));
+		}
+	}
+}
